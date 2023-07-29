3e10e8cd08634977981f8b05d7ebed0ed346eb90
Ajay481 <iamajayyadav481@gmail.com>
2023-07-29 18:45:45 +0530

    removed linting error

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import ChatMessages from "./ChatMessages";
 import { useDispatch, useSelector } from "react-redux";
